54d46dc23eb9885a61ea78e0ea514fd41997cf03
Wessel Kronemeijer <wessel32@gmail.com>
2024-05-16 17:08:27 +0200

    Inline assertions

--- Core/Assert.cs
+++ Core/Assert.cs
@@ -2,82 +2,130 @@ namespace Core;
 
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 static class Contractual {
-    [DebuggerHidden]
-    [StackTraceHidden]
-    public static void Assert([DoesNotReturnIf(false)] bool condition) {
-        Assert(condition, "assertion failed");
-    }
+    public sealed class AssertionException(string reason) : Exception(reason);
 
     [DebuggerHidden]
     [StackTraceHidden]
-    public static void Assert([DoesNotReturnIf(false)] bool condition, string message) {
-        Assert(condition, () => message);
-    }
-
-    [DebuggerHidden]
-    [StackTraceHidden]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Assert(
-        [DoesNotReturnIf(false)] bool condition,
+        [DoesNotReturnIf(false)]
+        bool condition
+    ) {
+        if (!condition) {
+            throw new AssertionException("assertion failed");
+        }
+    }
+
+    [DebuggerHidden]
+    [StackTraceHidden]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Assert(
+        [DoesNotReturnIf(false)]
+        bool condition,
+        string message
+    ) {
+        if (!condition) {
+            throw new AssertionException(message);
+        }
+    }
+
+    [DebuggerHidden]
+    [StackTraceHidden]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Assert(
+        [DoesNotReturnIf(false)]
+        bool condition,
         Func<string> message
     ) {
         if (!condition) {
-            throw new Exception(message());
+            throw new AssertionException(message());
         }
     }
 
     [DebuggerHidden]
     [StackTraceHidden]
-    public static void Requires([DoesNotReturnIf(false)] bool condition) {
-        Requires(condition, "pre-condition failed");
-    }
-
-    [DebuggerHidden]
-    [StackTraceHidden]
-    public static void Requires([DoesNotReturnIf(false)] bool condition, string message) {
-        Requires(condition, () => message);
-    }
-
-    [DebuggerHidden]
-    [StackTraceHidden]
-    public static void Requires([DoesNotReturnIf(false)] bool condition, Func<string> message) {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Requires(
+        [DoesNotReturnIf(false)]
+        bool condition
+    ) {
         if (!condition) {
-            throw new Exception(message());
+            throw new AssertionException("pre-condition not met");
         }
     }
 
     [DebuggerHidden]
     [StackTraceHidden]
-    public static void Ensures([DoesNotReturnIf(false)] bool condition) {
-        Ensures(condition, "post-condition failed");
-    }
-
-    [DebuggerHidden]
-    [StackTraceHidden]
-    public static void Ensures([DoesNotReturnIf(false)] bool condition, string message) {
-        Ensures(condition, () => message);
-    }
-
-    [DebuggerHidden]
-    [StackTraceHidden]
-    public static void Ensures([DoesNotReturnIf(false)] bool condition, Func<string> message) {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Requires(
+        [DoesNotReturnIf(false)]
+        bool condition,
+        string message
+    ) {
         if (!condition) {
-            throw new Exception(message());
+            throw new AssertionException(message);
         }
     }
 
     [DebuggerHidden]
     [StackTraceHidden]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Requires(
+        [DoesNotReturnIf(false)]
+        bool condition,
+        Func<string> message
+    ) {
+        if (!condition) {
+            throw new AssertionException(message());
+        }
+    }
+
+    [DebuggerHidden]
+    [StackTraceHidden]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Ensures(
+        [DoesNotReturnIf(false)]
+        bool condition
+    ) {
+        if (!condition) {
+            throw new AssertionException("post-condition not met");
+        }
+    }
+
+    [DebuggerHidden]
+    [StackTraceHidden]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Ensures(
+        [DoesNotReturnIf(false)]
+        bool condition,
+        string message
+    ) {
+        if (!condition) {
+            throw new AssertionException(message);
+        }
+    }
+
+    [DebuggerHidden]
+    [StackTraceHidden]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Ensures(
+        [DoesNotReturnIf(false)]
+        bool condition,
+        Func<string> message
+    ) {
+        if (!condition) {
+            throw new AssertionException(message());
+        }
+    }
+
+    [DebuggerHidden]
+    [StackTraceHidden]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     [DoesNotReturn]
     public static void Panic(string message) {
         throw new Exception(message);
-    }
-
-    [DebuggerHidden]
-    [StackTraceHidden]
-    public static void DoOnlyOnce(ref bool doneBefore) {
-        Requires(!doneBefore, "should only execute once");
-        doneBefore = true;
     }
 }
