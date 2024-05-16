6a13c91a4312d11a5573a609afa4752821c67480
Wessel Kronemeijer <wessel32@gmail.com>
2024-05-16 23:08:09 +0200

    Add exit code for modes

--- TypeLox.Cli/Program.cs
+++ TypeLox.Cli/Program.cs
@@ -6,7 +6,10 @@ public class Program(ICompiler compiler) : ProgramMode.IVisitor {
         _ => throw new Exception($"unknown backend '{options.Backend}'"),
     };
 
-    public void Visit(ProgramMode.Repl command) {
+    const int SUCCESS = 0;
+    const int FAILURE = -1;
+
+    public int Visit(ProgramMode.Repl command) {
         var interpreter = compiler.Upgrade();
         while (true) {
             try {
@@ -18,34 +21,39 @@ public class Program(ICompiler compiler) : ProgramMode.IVisitor {
                 Console.Error.WriteLine(e.ToString());
             }
         }
+        return SUCCESS;
     }
 
-    public void Visit(ProgramMode.ExecuteFile command) {
+    public int Visit(ProgramMode.ExecuteFile command) {
         var interpreter = compiler.Upgrade();
         try {
             interpreter.RunFile(command.FileUri);
+            return SUCCESS;
         } catch (LoxException e) {
             Console.Error.WriteLine(e.ToString());
+            return FAILURE;
         }
     }
 
     // TODO: Add compile to file mode
     // Funny to see how disjoint the feature sets between interpreter and transpiler really is
 
-    public void Visit(ProgramMode.TestDirectory value) {
+    public int Visit(ProgramMode.TestDirectory value) {
         var interpreter = compiler.Upgrade();
         var sources = interpreter.Host.FindTestFiles();
         var runner = new TestRunner(interpreter, sources);
         Console.WriteLine($"running {sources.Count} test(s)");
         runner.RunAllTests();
+        // TODO: Check for test results
+        return SUCCESS;
     }
 
-    public static void Main(string[] args) {
+    public static int Main(string[] args) {
         Console.InputEncoding = Console.OutputEncoding = Encoding.UTF8;
         var (mode, options) = ArgsParser.Parse(args);
         var host = new CompilerHost(options.CompilerOptions);
         var compiler = SelectBackend(host, options);
         host.WriteLine($"using backend '{compiler.Name}'");
-        mode.Accept(new Program(compiler));
+        return mode.Accept(new Program(compiler));
     }
 }
--- TypeLox.Cli/ProgramMode.cs
+++ TypeLox.Cli/ProgramMode.cs
@@ -2,31 +2,31 @@ namespace TypeLox.Cli;
 
 public abstract record class ProgramMode() {
     public interface IVisitor {
-        void Visit(Repl value);
-        void Visit(ExecuteFile value);
-        void Visit(TestDirectory value);
+        int Visit(Repl value);
+        int Visit(ExecuteFile value);
+        int Visit(TestDirectory value);
     }
 
-    public abstract void Accept(IVisitor visitor);
+    public abstract int Accept(IVisitor visitor);
 
     /// <summary>
     /// Runs the program as an interactive prompt.
     /// </summary>
     public record class Repl() : ProgramMode() {
-        public override void Accept(IVisitor visitor) => visitor.Visit(this);
+        public override int Accept(IVisitor visitor) => visitor.Visit(this);
     }
 
     /// <summary>
     /// Reads the file, then executes it, then terminates.
     /// </summary>
     public record class ExecuteFile(Uri FileUri) : ProgramMode() {
-        public override void Accept(IVisitor visitor) => visitor.Visit(this);
+        public override int Accept(IVisitor visitor) => visitor.Visit(this);
     }
 
     /// <summary>
     /// Finds all files ./**/*.test.lox and runs them for as tests.
     /// </summary>
     public record class TestDirectory() : ProgramMode() {
-        public override void Accept(IVisitor visitor) => visitor.Visit(this);
+        public override int Accept(IVisitor visitor) => visitor.Visit(this);
     }
 }
